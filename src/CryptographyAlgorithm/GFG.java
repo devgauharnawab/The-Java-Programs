1d9b1a73c219d4acd20bed47eb2694329d29ff9c
Gauhar Nawab <74108518+gauharnawab@users.noreply.github.com>
2022-12-08 17:46:56 +0530

    Update GFG.java

--- src/CryptographyAlgorithm/GFG.java
+++ src/CryptographyAlgorithm/GFG.java
@@ -4,7 +4,7 @@ import java.math.BigInteger;
 import  java.security.MessageDigest;
 import  java.security.NoSuchAlgorithmException;
 
-public class GFG {
+public class CryptographyAlgo {
     public static String encryptThisString(String input){
         try {
             // getInstance() method is called with algorithm SHA-1
